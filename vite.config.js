2ff45ba15faf76d0d8280aca45a2aaadfb8f6b15
infojihadsheikh-a11y <info.jihadsheikh@gmail.com>
2026-02-08 18:02:33 +0600

    updated config to run in spacific port

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      port: parseInt(env.PORT) || 5173,
+    },
+  }
 })
